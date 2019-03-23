1e20c4caa0700ea25e16270816eac305e660e366
MK <manasasrinivas.k@gmail.com>
2019-03-23 21:57:23 +0000

    Swap 2 numbers

--- Hellowworld.java
+++ Hellowworld.java
@@ -5,5 +5,28 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
-    } 
-} 
+   }}
+import java.util.Scanner;
+ 
+class SwapNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, temp;
+      System.out.println("Enter x and y");
+      Scanner in = new Scanner(System.in);
+     
+      x = in.nextInt();
+      y = in.nextInt();
+     
+      System.out.println("Before Swapping\nx = "+x+"\ny = "+y);
+     
+      temp = x;
+      x = y;
+      y = temp;
+     
+      System.out.println("After Swapping\nx = "+x+"\ny = "+y);
+   }
+}
+
+ 
